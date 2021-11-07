b3ff74e89688df381bbb9b123776c8f8962b0401
it18011876@my.sliit.lk <it18011876@my.sliit.lk>
2021-11-07 19:18:47 +0530

    init nodejs backend api

---
+++ api/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req,res) => {
+    res.json('API is working!!!!');
+});
+
+//Start server
+app.listen(PORT, () => {
+    console.log('API is listening on PORT: ${PORT}');
+});
